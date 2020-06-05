2efb32d128bb7b073d0b906363b2858fa59d56ca
moonheuiwon <wonheuimoon@gmail.com>
2020-06-05 16:00:48 +0900

    베이스 엑티비티에서 커스텀 액션바 사용 처리  - 노액션바인 경우에는 건너뛰도록 처리

--- app/src/main/java/kr/co/tjoeun/colosseum_kotlin/BaseActivity.java
+++ app/src/main/java/kr/co/tjoeun/colosseum_kotlin/BaseActivity.java
@@ -1,9 +1,13 @@
 package kr.co.tjoeun.colosseum_kotlin;
 
 import android.content.Context;
+import android.os.Bundle;
+import android.os.PersistableBundle;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public abstract class BaseActivity extends AppCompatActivity {
 
@@ -12,8 +16,24 @@ public abstract class BaseActivity extends AppCompatActivity {
     public abstract void setupEvents();
     public abstract void setValues();
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+    }
+
     public void setCustomActionBar() {
-        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+
+        if (getSupportActionBar() != null) {
+
+            getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+            getSupportActionBar().setCustomView(R.layout.custom_action_bar);
+
+            getSupportActionBar().setBackgroundDrawable(null);
+
+            Toolbar toolbar = (Toolbar) getSupportActionBar().getCustomView().getParent();
+            toolbar.setContentInsetsAbsolute(0,0);
+        }
     }
 
 }
